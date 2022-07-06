261b3c73b6369436f1deea3cc4f56209c01b1c82
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-07-06 21:34:39 +1200

    Add questions file

---
+++ QUESTIONS.js
@@ -0,0 +1,10 @@
+/* 
+! 1. If I change action/index.js file name to anything
+eg: actionCreator.js
+and update all the imports gives an error of cannot find module '../actions' 
+on Home component line 48
+which is the export of Home so no clue there...
+Am I not allowed to chnage the file name or whats up??
+
+
+*/
